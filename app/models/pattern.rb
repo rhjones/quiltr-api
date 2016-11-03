09485fb49358004cb514028c1fd298ac898c4caf
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-03 10:19:13 -0400

    Add S3 settings and upload files to S3
    Add config settings in config/environments/dev & prod
    Use aws-sdk gem instead of aws-sdk-v1

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -2,7 +2,6 @@ class Pattern < ActiveRecord::Base
   has_attached_file :upload, :default_url => '/images/no-pattern-image.png'
   validates_attachment_content_type :upload, :content_type => /\Aimage\/.*\Z/
 
-
   # By default, every file uploaded will be named 'data'.
   # This renames the file to a timestamp, preventing name collisions.
   # This will also fix browser caching issues for updates
