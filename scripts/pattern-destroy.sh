72c3d1e81dd28bbb60ed570258cdaf79b85a1f81
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-05 09:26:40 -0400

    Add curl scripts for patterns
    Add pattern-destroy and pattern_upload-show
    Also: change localhost port for auth scripts to 4741

---
+++ scripts/pattern-destroy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl --include --request DELETE http://localhost:4741/patterns/$ID
--- scripts/user.sh
+++ scripts/user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --include --request GET http://localhost:3000/users/$ID \
+curl --include --request GET http://localhost:4741/users/$ID \
   --header "Authorization: Token token=$TOKEN"
