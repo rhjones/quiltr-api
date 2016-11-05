f61c5c84d894cb99b286044884cc4a82a3f263f7
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-05 09:32:29 -0400

    Remove old version of pattern upload create script
    Delete commented out code

--- scripts/pattern_upload-create.sh
+++ scripts/pattern_upload-create.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# curl --include --request POST http://localhost:4741/patterns \
-#   --header "Content-Type: application/json" \
-#   -F "upload=@demo.png;type=image/png" \
-#   --data '{
-#     "pattern": {
-#       "quilt_size": "twin",
-#     }
-#   }'
-
 curl -v http://localhost:4741/pattern_uploads \
   -H 'Content-Type: multipart/form-data' \
   -H 'Accept: application/json' \
