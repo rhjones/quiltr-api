f65cd54d07f86488593b0e5b5cf5d744b0b525ab
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-07 17:28:13 -0500

    Add uploads to project serializer
    Add uploads to project serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,12 +1,12 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :notes, :start_date, :finish_date, :finished
-  # has_many :project_uploads
+  has_many :project_uploads
   belongs_to :pattern
   belongs_to :user
 
-  # def project_uploads
-  #   object.project_uploads.pluck(:id)
-  # end
+  def project_uploads
+    object.project_uploads.pluck(:id)
+  end
 
   def user
     object.user.id
