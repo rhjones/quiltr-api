898207af3280519c0a8fe6215f3b2c2172cecaca
rhjones <rebekah.heacock@gmail.com>
2016-11-30 11:15:22 -0500

    Permit requests from multiple localhost ports
    Allow requests not only from localhost:4741 but from any localhost:####
    This enables local development of multiple apps that consume Quiltr API data

--- config/application.rb
+++ config/application.rb
@@ -40,10 +40,10 @@ module QuiltrApi
 
     # Cross-Origin Resource Sharing
     # development client port
-    cors_port = 'GA'.each_byte.reduce('') { |a, e| a + format('%d', e) }.to_i
+    # cors_port = 'GA'.each_byte.reduce('') { |a, e| a + format('%d', e) }.to_i
     config.middleware.use Rack::Cors do
       allow do
-        origins ENV['CLIENT_ORIGIN'] || "http://localhost:#{cors_port}"
+        origins ENV['CLIENT_ORIGIN'] || %r|http://localhost:\d{4}|
         resource '*',
                  headers: :any,
                  methods: [:options, :get,
